d08b0cfcc452e4f07d11d1a592515bc5aab03343
codeamazone <e.niggeme@gmail.com>
2021-02-03 15:37:55 +0100

    updated AuthorController, created Movie and MovieController

--- resources/views/public/authors/show.blade.php
+++ resources/views/public/authors/show.blade.php
@@ -6,7 +6,9 @@
 @section('content')
     <div class="align-content-center">
         <h5>{{ $author->id }} {{ $author->firstname }} {{ $author->lastname }}</h5>
+        <h6>Anzahl Filme: {{ $author->movies->count() }} </h6>
         <div>
+            <!-- Gib alle movie-Titel des Autoren aus -->
         </div>
     </div>
 @endsection

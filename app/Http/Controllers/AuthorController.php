a4369e2dee29e896f227bd5219dc135f6d52963a
codeamazone <e.niggeme@gmail.com>
2021-02-03 12:34:01 +0100

    controllers and models

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Author;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class AuthorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param Author $author
+     * @return Response
+     */
+    public function show(Author $author)
+    {
+        //
+    }
+
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param Author $author
+     * @return Response
+     */
+    public function edit(Author $author)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param Author $author
+     * @return Response
+     */
+    public function update(Request $request, Author $author)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param Author $author
+     * @return Response
+     */
+    public function destroy(Author $author)
+    {
+        //
+    }
+}

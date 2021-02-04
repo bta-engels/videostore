f2ee09f50e00288ca6ac26ab32f631facba7c48b
berndengels <engels@f50.de>
2021-02-04 15:40:00 +0100

    authors crud

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -2,8 +2,34 @@
 
 namespace App\Models;
 
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
+/**
+ * App\Models\Movie
+ *
+ * @property int $id
+ * @property int $author_id
+ * @property string $title
+ * @property string $price
+ * @property string|null $image
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read Author $author
+ * @method static Builder|Movie newModelQuery()
+ * @method static Builder|Movie newQuery()
+ * @method static Builder|Movie query()
+ * @method static Builder|Movie whereAuthorId($value)
+ * @method static Builder|Movie whereCreatedAt($value)
+ * @method static Builder|Movie whereId($value)
+ * @method static Builder|Movie whereImage($value)
+ * @method static Builder|Movie wherePrice($value)
+ * @method static Builder|Movie whereTitle($value)
+ * @method static Builder|Movie whereUpdatedAt($value)
+ * @mixin Eloquent
+ */
 class Movie extends Model
 {
 	protected $table = 'movies';

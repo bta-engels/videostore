0709e7ca9da067efcc9a721855e1446c95ad58ba
berndengels <engels@f50.de>
2021-02-04 10:32:45 +0100

    stuff

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Movie extends Model
 {
-    use HasFactory;
-
 	protected $table = 'movies';
+
+	public function author()
+    {
+        return $this->belongsTo(Author::class);
+    }
 }
--- resources/views/public/authors/show.blade.php
+++ resources/views/public/authors/show.blade.php
@@ -9,6 +9,9 @@
         <h6>Anzahl Filme: {{ $author->movies->count() }}</h6>
         <div>
             <!-- gib alle movie titel aus -->
+            <ul>
+                <li></li>
+            </ul>
         </div>
     </div>
 @endsection

16a296a5c0b4a14ec07917e9cf421ab32365e40a
codeamazone <e.niggeme@gmail.com>
2021-02-04 11:06:04 +0100

    updated show.blade.php

--- resources/views/public/authors/show.blade.php
+++ resources/views/public/authors/show.blade.php
@@ -5,13 +5,20 @@
 
 @section('content')
     <div class="align-content-center">
-        <h5>{{ $author->id }} {{ $author->firstname }} {{ $author->lastname }}</h5>
+        <h5 style="color:darkturquoise"><b>{{ $author->id }}: {{ $author->firstname }} {{ $author->lastname }}</b></h5>
         <h6>Anzahl Filme: {{ $author->movies->count() }}</h6>
+        <h5>Filme:</h5>
         <div>
             <!-- Gib alle movie-Titel des Autoren aus -->
             <ul>
-                @foreach($author->movies as $movie)
-                <li>$movie->title</li>
+{{--                @foreach($author->movies as $movie)--}}
+{{--                <li>{{ $movie->title }}</li>--}}
+{{--                @endforeach--}}
+                @forelse($author->movies as $movie)
+                    <li>{{ $movie->title }}</li>
+                    @empty
+                    <p>Dieser Autor hat keine Filme</p>
+                @endforelse
             </ul>
         </div>
     </div>

173102777e29b2a25f27d4f6ed7c292a66acb805
lena <lena@example.com>
2021-02-05 09:32:48 +0100

    change

---
+++ database/migrations/2021_02_05_075913_create_todos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTodosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('todos', function (Blueprint $table) {
+            $table->id();
+            $table->boolean("done")->default(false);
+            $table->string(text,50);
+            $table->timestamp(created_at);
+            $table->timestamp(updated_at);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('todos', function (Blueprint $table) {
+            //
+        });
+    }
+}

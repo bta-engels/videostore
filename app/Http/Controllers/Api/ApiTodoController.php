addaf0225ee8c4e92d3e3f1d03c90468ab3a2158
berndengels <engels@f50.de>
2021-02-10 16:33:14 +0100

    api route

---
+++ app/Http/Controllers/Api/ApiTodoController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Todo;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ApiTodoRequest;
+
+class ApiTodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = Todo::all();
+        // @todo: add ressoure class here
+        return response()->json($data);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Todo $todo)
+    {
+        return response()->json($todo);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(ApiTodoRequest $request)
+    {
+        $todo = Todo::create($request->validated());
+        return response()->json($todo);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(ApiTodoRequest $request, Todo $todo)
+    {
+        $todo->update($request->validated());
+        return response()->json($todo);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Todo $todo)
+    {
+        $todo->delete();
+        return response()->json($todo);
+    }
+}

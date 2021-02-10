240854e1a21bed637c442a4c5b69f1d30091b58a
berndengels <engels@f50.de>
2021-02-10 09:47:25 +0100

    todos

---
+++ app/Http/Requests/TodoRequest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+/**
+ * Class AuthorRequest
+ * @package App\Http\Requests
+ */
+class TodoRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return Auth::check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            // Alles Plichtfelder und mindestens 3 Zeichen lang
+            'text'  => 'required|min:3|max:50',
+            'done'  => '',
+        ];
+    }
+
+    /**
+     * Gib mir meine eigene Fehlermeldungen aus
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'text.required'    => 'Bitte einen Text eingeben',
+            'text.min'         => 'Der Text muß mindesten :min Zeichen enthalten',
+            'text.max'         => 'Der Text darf maximal :max Zeichen enthalten',
+        ];
+    }
+
+}

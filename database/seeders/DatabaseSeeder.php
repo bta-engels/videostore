a0b287273ca269e9d4f2c155252475298baef281
berndengels <engels@f50.de>
2021-02-15 16:11:13 +0100

    add db seeders, set foreign key in movies

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,10 +3,10 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
 
 class DatabaseSeeder extends Seeder
 {
-
     /**
      * Seed the application's database.
      *
@@ -14,6 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        Schema::disableForeignKeyConstraints();
+        $this->call([
+            AuthorSeeder::class,
+            MovieSeeder::class,
+            TodoSeeder::class,
+        ]);
+        Schema::enableForeignKeyConstraints();
     }
 }
